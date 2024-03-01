6f6274f88f2a1cf6b97da8042ec6801518e78db2
SilasRoberts <142546763+SilasRoberts@users.noreply.github.com>
2024-03-01 17:57:05 +0000

    Fri, Mar 1, 2024, 9:57 AM -08:00

--- js/sound.js
+++ js/sound.js
@@ -5,4 +5,6 @@ sounds.forEach(sound =>{
     btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerText=sound
+
+    document.getElementById('buttons').appendChild(btn)
 })
