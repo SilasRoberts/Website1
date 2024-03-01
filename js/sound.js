fb89b2a8dae92b34afec98010e1b5d3c5fa8f0be
SilasRoberts <142546763+SilasRoberts@users.noreply.github.com>
2024-03-01 18:12:19 +0000

    Fri, Mar 1, 2024, 10:12 AM -08:00

--- js/sound.js
+++ js/sound.js
@@ -1,18 +1,19 @@
-sounds=['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 
-sounds.forEach(sound =>{
+sounds.forEach(sound => {
     btn = document.createElement('button')
     btn.classList.add('btn')
-    btn.innerText=sound
+    btn.innerText = sound
     btn.addEventListener('click', () => {
         stopSongs()
         document.getElementById(sound).play()
     })
     document.getElementById('buttons').appendChild(btn)
 })
-function stopSongs(){
-    sounds.forEach(sound =>{
+
+function stopSongs() {
+    sounds.forEach(sound => {
         song = document.getElementById(sound)
         song.pause()
         song.currentTime = 0
