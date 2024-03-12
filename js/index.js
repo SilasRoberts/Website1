68cf6ef50f10092a3936a90905f7846ec56ae710
SilasRoberts <142546763+SilasRoberts@users.noreply.github.com>
2024-03-12 17:17:36 +0000

    Tue, Mar 12, 2024, 10:17 AM -07:00

--- js/index.js
+++ js/index.js
@@ -6,9 +6,10 @@ function wakeUp() {
     var button = document.getElementById("wakeUpButton");
     var image = document.getElementById("image");
     var links = document.getElementById("links");
+    var background = document.querySelector(".background");
     button.disabled = true;
     button.style.opacity = "0";
     image.style.transition = "opacity 2s";
-    image.style.opacity = "0";
-    image.style.backgroundColor = "black";
-    setTimeout(function() { links.style.display = "block"; }, 2000); }
+    background.style.opacity = "1";
+    image.style.opacity = "0"; setTimeout(function() { links.style.display = "block"; }, 2000);
+}
