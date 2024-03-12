e22638fb775a7cf63f6e4c3199e4a368ff6699af
SilasRoberts <142546763+SilasRoberts@users.noreply.github.com>
2024-03-12 16:52:15 +0000

    Tue, Mar 12, 2024, 9:52 AM -07:00

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-document.getElementById("showWebsitesBtn").addEventListener("click", function() {
-    document.getElementById("overlay").style.backgroundColor = "rgba(0, 0, 0, 0.8)";
-    document.querySelector(".websites").classList.add("fade-in");
-
-
+function wakeUp() {
+    var button = document.getElementById("wakeUpButton");
+    var image = document.getElementById("image");
+    var links = document.getElementById("links");
+    button.style.display = "none";
+    image.style.transition = "opacity 2s";
+    image.style.opacity = "0";
     setTimeout(function() {
-        document.querySelector(".image-container").style.display = "none";
-    }, 1000);
-});
+        links.style.display = "block";
+    }, 2000); // 2 seconds }
